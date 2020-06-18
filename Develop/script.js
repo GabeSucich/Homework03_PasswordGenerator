9063debc7dda7c7e542f9ba184e97ce75b4f6f18
Gabe Sucich <gabe.sucich@gmail.com>
2020-06-18 13:31:13 -0600

    Adding a prompter function what will handle asking the user which criteria need to be included.

--- Develop/script.js
+++ Develop/script.js
@@ -13,10 +13,42 @@ function writePassword() {
 function generatePassword() {
   // The main password-generating function.
 
+  // Each of the four possible character sets.
   var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var uppercase = lowercase.map(letter => letter.toUpperCase())
   var numericals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   var specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '<', '<', '?'];
+
+  // An object for each criterion.
+  var has_lowercase = { include: false, charSet: lowercase, charNum: 0 };
+  var has_uppercase = { include: false, charSet: uppercase, charNum: 0 };
+  var has_numericals = { include: false, charSet: numericals, charNum: 0 };
+  var has_specials = { include: false, charSet: specials, charNum: 0 };
+
+
+  function criteriaPrompt(criteria_name, criteria_object) {
+    // Takes in the name of the criteria for display, and the associated criteria object. This function will prompt the user to include or exclude each of the four criteria.
+
+    response = prompt("Would you like your password to include " + criteria_name + "? Type 'yes' or 'no'.");
+
+    if (response.toLowerCase() === 'yes') {
+      criteria_object.include = true;
+      criteria_number += 1;
+    }
+
+    else if (response.toLowerCase() === 'no') {
+      criteria_object.include = false;
+    }
+
+    else {
+      alert("That is not a valid response. Please type 'yes' or 'no'.")
+      criteriaPrompt(criteria_name, criteria_object)
+    };
+
+
+  };
+
+
 
 }
 
