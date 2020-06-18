1cc2e56aa45b1c90368b54f9271e7a33d509191e
Gabe Sucich <gabe.sucich@gmail.com>
2020-06-18 13:34:00 -0600

    Adding function that prompts the user to input a password length

--- Develop/script.js
+++ Develop/script.js
@@ -25,6 +25,9 @@ function generatePassword() {
   var has_numericals = { include: false, charSet: numericals, charNum: 0 };
   var has_specials = { include: false, charSet: specials, charNum: 0 };
 
+  // Variable storing the password length.
+  var pass_length = 0;
+
 
   function criteriaPrompt(criteria_name, criteria_object) {
     // Takes in the name of the criteria for display, and the associated criteria object. This function will prompt the user to include or exclude each of the four criteria.
@@ -46,6 +49,32 @@ function generatePassword() {
     };
 
 
+  };
+
+  function setLength() {
+    // Function which when called gets the user to set a password length.
+
+    response = prompt('How many characters would you like your password to be? Type a number. (Passwords can be betwen 8 and 128 characters long.')
+    response = parseInt(response);
+    console.log(response)
+
+    if (Number.isInteger(response)) {
+
+      if (response > 7 && response < 129) {
+        pass_length = response;
+      }
+
+      else {
+        alert("Password length can be no less than 8 and no greater than 128 characters long.");
+        setLength();
+      }
+
+    }
+
+    else {
+      alert("The length you enter must be a number.");
+      setLength();
+    };
   };
 
 
